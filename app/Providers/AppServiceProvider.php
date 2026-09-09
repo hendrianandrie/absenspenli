ed18ae355163a76f0ad838b23f129c06c24d1a1c
ndrie <ndrie@ndries.local>
2026-09-10 05:35:46 +0700

    Fix timezone to Asia/Jakarta and locale to Indonesian (id)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        date_default_timezone_set(config('app.timezone', 'Asia/Jakarta'));
+        \Carbon\Carbon::setLocale(config('app.locale', 'id'));
     }
 }
